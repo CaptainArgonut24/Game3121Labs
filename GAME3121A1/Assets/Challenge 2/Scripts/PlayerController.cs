9bf42a962275a4f4c292d879edbcb755a03c5adf
John <johnhusky400@gmail.com>
2024-10-03 17:53:22 -0400

    done part 2

---
+++ GAME3121A1/Assets/Challenge 2/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float actuationDelay;
+
+    private float elaspedTime;
+    void Update()
+    {
+        elaspedTime += Time.deltaTime;
+        if (elaspedTime > actuationDelay)
+            // Spacebar spawns dog
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                elaspedTime = 0;
+            }
+        }
+
+    }
+}
